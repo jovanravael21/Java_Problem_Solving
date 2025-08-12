b4986df115a77d482cb8f96a995f4a371e3a9b10
jovanravael21 <jovanravael21@gmail.com>
2025-08-12 21:31:58 +1000

    new file

---
+++ src/Fibonacci.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Fibonacci {
+    public static void main(String[] args){
+        Scanner scnr = new Scanner(System.in);
+        System.out.print("Enter Number of Fibonacci Numbers: ");
+        int n = scnr.nextInt();
+        int a = 0;
+        int b = 1;
+        int c;
+
+        for(int i = 0; i < n; i++){
+            if(i < 2){
+                System.out.println(i);
+            }else{
+                c = a + b;
+                System.out.println(c);
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
